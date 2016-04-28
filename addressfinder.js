c7463c0401bcc2eb86dc054c78a30cc1af7aeb07
Fiona Sanggang <fsanggang@gmail.com>
2016-04-29 10:04:16 +1200

    Refactor: Reference nz.code and au.code in addressWidget()

--- addressfinder.js
+++ addressfinder.js
@@ -30,13 +30,13 @@
     }
 
     if (nz.key) {
-      widgetNZ = hydrate(prefix, nz.key, 'NZ', selectNewZealand);
+      widgetNZ = hydrate(prefix, nz.key, nz.code, selectNewZealand);
     } else {
       widgetNZ = nullWidget;
     }
 
     if (au.key) {
-      widgetAU = hydrate(prefix, au.key, 'AU', selectAustralia);
+      widgetAU = hydrate(prefix, au.key, au.code, selectAustralia);
     } else {
       widgetAU = nullWidget;
     }
