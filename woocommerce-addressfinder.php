858bc47e49ea13d9851418f5e7835f9b32305a7f
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2016-11-09 17:42:07 +1300

    increase z-index to stop the popup from being obscured

--- woocommerce-addressfinder.php
+++ woocommerce-addressfinder.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: AddressFinder
 Plugin URI: https://github.com/Abletech/woocommerce-addressfinder
-Version: 1.0.12
+Version: 1.0.13
 Author: Abletech
 Description: Woocommerce address finder plugin for autocompleting addresses in New Zealand and Australia
  */
