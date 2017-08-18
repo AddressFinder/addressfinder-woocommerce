a77300bc67881420852f82f7f44cc22684238c1f
katenorquay <katenorquay@live.com>
2017-08-18 16:11:21 +1200

    fields populating correctly for both countries

--- dist/addressfinder.js
+++ dist/addressfinder.js
@@ -157,22 +157,17 @@ var WooCommercePlugin = function () {
     this.initialisePlugin();
   }
 
+  // initialiseWidget() {
+  //   widget._getPosition = function(){
+  //     var coords = $(this.element).offset();
+  //     coords.top += $(this.element).outerHeight();
+  //     return coords;
+  //   };
+  //
+  //   return widget;
+  // };
+
   _createClass(WooCommercePlugin, [{
-    key: 'initialiseWidget',
-    value: function initialiseWidget(prefix, key, code, onSelectFn, widgetOptions) {
-      var widget = new window.AddressFinder.Widget(document.getElementById(prefix + 'address_1'), key, code, widgetOptions);
-
-      widget.on('result:select', onSelectFn);
-
-      widget._getPosition = function () {
-        var coords = $(this.element).offset();
-        coords.top += $(this.element).outerHeight();
-        return coords;
-      };
-
-      return widget;
-    }
-  }, {
     key: 'bindToAddressPanel',
     value: function bindToAddressPanel(panelPrefix) {
       var that = this;
@@ -184,10 +179,12 @@ var WooCommercePlugin = function () {
 
       var widgets = {};
 
-      widgets.nz = this.initialiseWidget(panelPrefix, this.widgetConfig.nzKey, 'nz', this.selectNewZealand, this.widgetConfig.nzWidgetOptions);
-      widgets.au = this.initialiseWidget(panelPrefix, this.widgetConfig.auKey, 'au', this.selectAustralia, this.widgetConfig.auWidgetOptions);
-
+      widgets.nz = new window.AddressFinder.Widget(document.getElementById(panelPrefix + 'address_1'), this.widgetConfig.nzKey, 'nz', this.widgetConfig.nzWidgetOptions);
+      widgets.nz.on('result:select', this.selectNewZealand.bind(this));
       widgets.nz.prefix = panelPrefix;
+
+      widgets.au = new window.AddressFinder.Widget(document.getElementById(panelPrefix + 'address_1'), this.widgetConfig.auKey, 'au', this.widgetConfig.auWidgetOptions);
+      widgets.au.on('result:select', this.selectAustralia.bind(this));
       widgets.au.prefix = panelPrefix;
 
       var countryChangeHandler = function countryChangeHandler(clear) {
@@ -250,98 +247,51 @@ var WooCommercePlugin = function () {
       }
     }
   }, {
-    key: 'selectNewZealand',
-    value: function selectNewZealand(a, metaData) {
-      console.log(a, metaData);
-      var prefix = this.prefix;
-
-      /* clear address fields */
-      // clearFields(prefix);
-
-      /* split and trim */
-      var address = metaData.postal || metaData.a;
-      var addressLines = address.split(',');
-      for (var i = 0; i < addressLines.length; i++) {
-        addressLines[i] = addressLines[i].replace(/^\s+|\s+$/g, '');
-      }
-
-      /* remove City/Postcode */
-      var city = metaData.mailtown || metaData.city;
-      if (addressLines[addressLines.length - 1] == city + ' ' + metaData.postcode) {
-        addressLines.pop();
-        this.setFieldValue(prefix + 'city', city);
-        this.setFieldValue(prefix + 'postcode', metaData.postcode);
-      }
-
-      /* set address2 */
-      if (addressLines.length > 1 && checkFieldPresent(prefix, 'address_2')) {
-        this.setFieldValue(prefix + 'address_2', addressLines.pop());
-      } else {
-        this.setFieldValue(prefix + 'address_2', '');
-      }
-
-      /* set address1 */
-      this.setFieldValue(prefix + 'address_1', addressLines.join(', '));
-
-      var region_code = {
-        'Auckland Region': 'AK',
-        'Bay Of Plenty Region': 'BP',
-        'Canterbury Region': 'CT',
-        'Gisborne Region': 'GI',
-        'Hawke\'s Bay Region': 'HB',
-        'Manawatu-Wanganui Region': 'MW',
-        'Marlborough Region': 'MB',
-        'Nelson Region': 'NS',
-        'Northland Region': 'NL',
-        'Otago Region': 'OT',
-        'Southland Region': 'SL',
-        'Taranaki Region': 'TK',
-        'Tasman Region': 'TM',
-        'Waikato Region': 'WA',
-        'Wellington Region': 'WE',
-        'West Coast Region': 'WC',
-        'No Region (Chatham Islands)': null
-      }[metaData.region];
-
-      this.selectState(prefix, region_code);
-    }
-  }, {
     key: 'selectAustralia',
     value: function selectAustralia(address, metaData) {
-      var prefix = this.prefix;
+      var prefix = 'billing_';
 
       // Set fields to new values
       if (metaData.address_line_2 != null) {
-        if (checkFieldPresent(prefix, 'address_2')) {
-          this.setFieldValue(prefix + 'address_1', metaData.address_line_1);
-          this.setFieldValue(prefix + 'address_2', metaData.address_line_2);
+        if (this.checkFieldPresent(prefix, 'address_2')) {
+          this._setElementValue(prefix + 'address_1', metaData.address_line_1);
+          this._setElementValue(prefix + 'address_2', metaData.address_line_2);
         } else {
           var combinedAddressLine1And2 = metaData.address_line_1 + ', ' + metaData.address_line_2;
-          this.setFieldValue(prefix + 'address_1', combinedAddressLine1And2);
+          this._setElementValue(prefix + 'address_1', combinedAddressLine1And2);
         }
       } else {
-        this.setFieldValue(prefix + 'address_1', metaData.address_line_1);
-        this.setFieldValue(prefix + 'address_2', '');
+        this._setElementValue(prefix + 'address_1', metaData.address_line_1);
+        this._setElementValue(prefix + 'address_2', '');
       }
 
-      this.setFieldValue(prefix + 'city', metaData.locality_name || '');
+      this._setElementValue(prefix + 'city', metaData.locality_name || '');
 
       this.selectState(prefix, metaData.state_territory);
-      this.setFieldValue(prefix + 'postcode', metaData.postcode);
+      this._setElementValue(prefix + 'postcode', metaData.postcode);
     }
   }, {
-    key: 'setFieldValue',
-    value: function setFieldValue(elementId, value) {
-      var field = document.getElementById(elementId);
-
-      if (field) {
-        field.value = value;
+    key: 'selectNewZealand',
+    value: function selectNewZealand(fullAddress, metaData) {
+      var prefix = this.prefix;
+      var selected = new AddressFinder.NZSelectedAddress(fullAddress, metaData);
+      this._setElementValue('billing_address_1', selected.address_line_1_and_2());
+      this._setElementValue('billing_address_2', selected.suburb());
+      this._setElementValue('billing_city', selected.city());
+      this._setElementValue('billing_postcode', selected.postcode());
+      this._setElementValue('billing_state_field', metaData.region);
+    }
+  }, {
+    key: '_setElementValue',
+    value: function _setElementValue(elementId, value) {
+      var element = document.getElementById(elementId);
+      if (element) {
+        element.value = value;
         return;
       }
-
       var errorMessage = 'AddressFinder Error - unable to find an element with id: ' + elementId;
 
-      if (this.widgetConfig.debug) {
+      if (true) {
         alert(errorMessage);
         return;
       }
@@ -350,6 +300,59 @@ var WooCommercePlugin = function () {
         window.console.log(errorMessage);
       }
     }
+
+    /* clear address fields */
+    // clearFields(prefix);
+
+    /* split and trim */
+    //   var address = metaData.postal || metaData.a;
+    //   var addressLines = address.split(',');
+    //   for(var i=0; i<addressLines.length; i++){
+    //     addressLines[i] = addressLines[i].replace(/^\s+|\s+$/g,'');
+    //   }
+    //
+    //   /* remove City/Postcode */
+    //   var city = metaData.mailtown || metaData.city;
+    //   if(addressLines[addressLines.length-1] == city + ' ' + metaData.postcode){
+    //     addressLines.pop();
+    //     this.setFieldValue(prefix + 'city', city);
+    //     this.setFieldValue(prefix + 'postcode', metaData.postcode);
+    //   }
+    //
+    //   /* set address2 */
+    //   if(addressLines.length > 1 && checkFieldPresent(prefix, 'address_2')){
+    //     this.setFieldValue(prefix + 'address_2', addressLines.pop());
+    //   }
+    //   else {
+    //     this.setFieldValue(prefix + 'address_2', '');
+    //   }
+    //
+    //   /* set address1 */
+    //   this.setFieldValue(prefix + 'address_1', addressLines.join(', '));
+    //
+    //   var region_code = {
+    //     'Auckland Region': 'AK',
+    //     'Bay Of Plenty Region': 'BP',
+    //     'Canterbury Region': 'CT',
+    //     'Gisborne Region': 'GI',
+    //     'Hawke\'s Bay Region': 'HB',
+    //     'Manawatu-Wanganui Region': 'MW',
+    //     'Marlborough Region': 'MB',
+    //     'Nelson Region': 'NS',
+    //     'Northland Region': 'NL',
+    //     'Otago Region': 'OT',
+    //     'Southland Region': 'SL',
+    //     'Taranaki Region': 'TK',
+    //     'Tasman Region': 'TM',
+    //     'Waikato Region': 'WA',
+    //     'Wellington Region': 'WE',
+    //     'West Coast Region': 'WC',
+    //     'No Region (Chatham Islands)': null
+    //   }[metaData.region];
+    //
+    //   this.selectState(prefix, region_code);
+    // };
+
   }, {
     key: 'initialisePlugin',
     value: function initialisePlugin() {
--- src/woocommerce_plugin.js
+++ src/woocommerce_plugin.js
@@ -13,24 +13,15 @@ export default class WooCommercePlugin {
     this.initialisePlugin()
   }
 
-  initialiseWidget(prefix, key, code, onSelectFn, widgetOptions) {
-    var widget = new window.AddressFinder.Widget(
-      document.getElementById(prefix + 'address_1'),
-      key,
-      code,
-      widgetOptions
-    );
-
-    widget.on('result:select', onSelectFn);
-
-    widget._getPosition = function(){
-      var coords = $(this.element).offset();
-      coords.top += $(this.element).outerHeight();
-      return coords;
-    };
-
-    return widget;
-  };
+  // initialiseWidget() {
+  //   widget._getPosition = function(){
+  //     var coords = $(this.element).offset();
+  //     coords.top += $(this.element).outerHeight();
+  //     return coords;
+  //   };
+  //
+  //   return widget;
+  // };
 
   bindToAddressPanel(panelPrefix){
     var that = this
@@ -42,10 +33,12 @@ export default class WooCommercePlugin {
 
     var widgets = {};
 
-    widgets.nz = this.initialiseWidget(panelPrefix, this.widgetConfig.nzKey, 'nz', this.selectNewZealand, this.widgetConfig.nzWidgetOptions);
-    widgets.au = this.initialiseWidget(panelPrefix, this.widgetConfig.auKey, 'au', this.selectAustralia, this.widgetConfig.auWidgetOptions);
-
+    widgets.nz = new window.AddressFinder.Widget(document.getElementById(panelPrefix + 'address_1'), this.widgetConfig.nzKey, 'nz', this.widgetConfig.nzWidgetOptions)
+    widgets.nz.on('result:select', this.selectNewZealand.bind(this));
     widgets.nz.prefix = panelPrefix;
+
+    widgets.au = new window.AddressFinder.Widget(document.getElementById(panelPrefix + 'address_1'), this.widgetConfig.auKey, 'au', this.widgetConfig.auWidgetOptions);
+    widgets.au.on('result:select', this.selectAustralia.bind(this));
     widgets.au.prefix = panelPrefix;
 
     var countryChangeHandler = function(clear){
@@ -110,96 +103,49 @@ export default class WooCommercePlugin {
     }
   };
 
-  selectNewZealand(a, metaData) {
-    console.log(a, metaData)
-    var prefix = this.prefix;
-
-    /* clear address fields */
-    // clearFields(prefix);
-
-    /* split and trim */
-    var address = metaData.postal || metaData.a;
-    var addressLines = address.split(',');
-    for(var i=0; i<addressLines.length; i++){
-      addressLines[i] = addressLines[i].replace(/^\s+|\s+$/g,'');
-    }
-
-    /* remove City/Postcode */
-    var city = metaData.mailtown || metaData.city;
-    if(addressLines[addressLines.length-1] == city + ' ' + metaData.postcode){
-      addressLines.pop();
-      this.setFieldValue(prefix + 'city', city);
-      this.setFieldValue(prefix + 'postcode', metaData.postcode);
-    }
-
-    /* set address2 */
-    if(addressLines.length > 1 && checkFieldPresent(prefix, 'address_2')){
-      this.setFieldValue(prefix + 'address_2', addressLines.pop());
-    }
-    else {
-      this.setFieldValue(prefix + 'address_2', '');
-    }
-
-    /* set address1 */
-    this.setFieldValue(prefix + 'address_1', addressLines.join(', '));
-
-    var region_code = {
-      'Auckland Region': 'AK',
-      'Bay Of Plenty Region': 'BP',
-      'Canterbury Region': 'CT',
-      'Gisborne Region': 'GI',
-      'Hawke\'s Bay Region': 'HB',
-      'Manawatu-Wanganui Region': 'MW',
-      'Marlborough Region': 'MB',
-      'Nelson Region': 'NS',
-      'Northland Region': 'NL',
-      'Otago Region': 'OT',
-      'Southland Region': 'SL',
-      'Taranaki Region': 'TK',
-      'Tasman Region': 'TM',
-      'Waikato Region': 'WA',
-      'Wellington Region': 'WE',
-      'West Coast Region': 'WC',
-      'No Region (Chatham Islands)': null
-    }[metaData.region];
-
-    this.selectState(prefix, region_code);
-  };
-
   selectAustralia(address, metaData) {
-    var prefix = this.prefix;
+    var prefix = 'billing_'
 
     // Set fields to new values
     if (metaData.address_line_2 != null) {
-      if (checkFieldPresent(prefix, 'address_2')) {
-        this.setFieldValue(prefix + 'address_1', metaData.address_line_1);
-        this.setFieldValue(prefix + 'address_2', metaData.address_line_2);
+      if (this.checkFieldPresent(prefix, 'address_2')) {
+        this._setElementValue(prefix + 'address_1', metaData.address_line_1);
+        this._setElementValue(prefix + 'address_2', metaData.address_line_2);
       } else {
         var combinedAddressLine1And2 = metaData.address_line_1 + ', ' + metaData.address_line_2;
-        this.setFieldValue(prefix + 'address_1', combinedAddressLine1And2);
+        this._setElementValue(prefix + 'address_1', combinedAddressLine1And2);
       }
     } else {
-      this.setFieldValue(prefix + 'address_1', metaData.address_line_1);
-      this.setFieldValue(prefix + 'address_2', '');
+      this._setElementValue(prefix + 'address_1', metaData.address_line_1);
+      this._setElementValue(prefix + 'address_2', '');
     }
 
-    this.setFieldValue(prefix + 'city', metaData.locality_name || '');
+    this._setElementValue(prefix + 'city', metaData.locality_name || '');
 
     this.selectState(prefix, metaData.state_territory);
-    this.setFieldValue(prefix + 'postcode', metaData.postcode);
+    this._setElementValue(prefix + 'postcode', metaData.postcode);
   };
 
-  setFieldValue(elementId, value) {
-    var field = document.getElementById(elementId);
 
-    if (field) {
-      field.value = value;
+  selectNewZealand(fullAddress, metaData) {
+    var prefix = this.prefix;
+    let selected = new AddressFinder.NZSelectedAddress(fullAddress, metaData);
+    this._setElementValue('billing_address_1', selected.address_line_1_and_2())
+    this._setElementValue('billing_address_2', selected.suburb())
+    this._setElementValue('billing_city', selected.city())
+    this._setElementValue('billing_postcode', selected.postcode())
+    this._setElementValue('billing_state_field', metaData.region)
+  }
+
+  _setElementValue(elementId, value){
+    var element = document.getElementById(elementId)
+    if (element) {
+      element.value = value;
       return;
     }
-
     var errorMessage = 'AddressFinder Error - unable to find an element with id: ' + elementId;
 
-    if (this.widgetConfig.debug) {
+    if (true) {
       alert(errorMessage);
       return;
     }
@@ -207,8 +153,59 @@ export default class WooCommercePlugin {
     if (window.console) {
       window.console.log(errorMessage);
     }
-  };
+  }
 
+    /* clear address fields */
+    // clearFields(prefix);
+
+    /* split and trim */
+  //   var address = metaData.postal || metaData.a;
+  //   var addressLines = address.split(',');
+  //   for(var i=0; i<addressLines.length; i++){
+  //     addressLines[i] = addressLines[i].replace(/^\s+|\s+$/g,'');
+  //   }
+  //
+  //   /* remove City/Postcode */
+  //   var city = metaData.mailtown || metaData.city;
+  //   if(addressLines[addressLines.length-1] == city + ' ' + metaData.postcode){
+  //     addressLines.pop();
+  //     this.setFieldValue(prefix + 'city', city);
+  //     this.setFieldValue(prefix + 'postcode', metaData.postcode);
+  //   }
+  //
+  //   /* set address2 */
+  //   if(addressLines.length > 1 && checkFieldPresent(prefix, 'address_2')){
+  //     this.setFieldValue(prefix + 'address_2', addressLines.pop());
+  //   }
+  //   else {
+  //     this.setFieldValue(prefix + 'address_2', '');
+  //   }
+  //
+  //   /* set address1 */
+  //   this.setFieldValue(prefix + 'address_1', addressLines.join(', '));
+  //
+  //   var region_code = {
+  //     'Auckland Region': 'AK',
+  //     'Bay Of Plenty Region': 'BP',
+  //     'Canterbury Region': 'CT',
+  //     'Gisborne Region': 'GI',
+  //     'Hawke\'s Bay Region': 'HB',
+  //     'Manawatu-Wanganui Region': 'MW',
+  //     'Marlborough Region': 'MB',
+  //     'Nelson Region': 'NS',
+  //     'Northland Region': 'NL',
+  //     'Otago Region': 'OT',
+  //     'Southland Region': 'SL',
+  //     'Taranaki Region': 'TK',
+  //     'Tasman Region': 'TM',
+  //     'Waikato Region': 'WA',
+  //     'Wellington Region': 'WE',
+  //     'West Coast Region': 'WC',
+  //     'No Region (Chatham Islands)': null
+  //   }[metaData.region];
+  //
+  //   this.selectState(prefix, region_code);
+  // };
   initialisePlugin(){
     if (document.getElementById('billing_address_1')){
       this.bindToAddressPanel('billing_');
