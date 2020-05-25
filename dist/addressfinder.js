19c3159f48792734ed3fd7797574cf37dbf84aa2
katenorquay <katenorquay@live.com>
2020-05-26 10:04:43 +1200

    update version numbers and changelog

--- dist/addressfinder.js
+++ dist/addressfinder.js
@@ -3005,7 +3005,7 @@ function woocommerce_plugin_createClass(Constructor, protoProps, staticProps) { 
     function WooCommercePlugin() {
       woocommerce_plugin_classCallCheck(this, WooCommercePlugin);
 
-      this.version = "1.4.1"; // Manages the mapping of the form configurations to the DOM.
+      this.version = "1.4.2"; // Manages the mapping of the form configurations to the DOM.
 
       this.PageManager = null; // Manages the form configurations, and creates any dynamic forms
 
