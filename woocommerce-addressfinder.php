1aa2d23b1e56e6556d72960afefe564f8a7ecca9
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2020-09-07 13:39:27 +1200

    Prepare for v1.4.5

--- woocommerce-addressfinder.php
+++ woocommerce-addressfinder.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: AddressFinder
 Plugin URI: https://github.com/Abletech/woocommerce-addressfinder
-Version: 1.4.4
+Version: 1.4.5
 Author: Abletech
 Description: Woocommerce address finder plugin for autocompleting addresses in New Zealand and Australia
  */
