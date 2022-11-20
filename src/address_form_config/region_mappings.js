5f89a2c29f74d98d5c4b5c8eea0909b40389b25d
Vincent Prigent <vincent.prigent1@gmail.com>
2022-11-21 09:19:46 +1300

    ADF-1963 Try to measure length of option values on region field

--- src/address_form_config/region_mappings.js
+++ src/address_form_config/region_mappings.js
@@ -1,19 +1,45 @@
-export default {
-  'Auckland Region': 'AK',
-  'Bay of Plenty Region': 'BP',
-  'Canterbury Region': 'CT',
-  'Gisborne Region': 'GI',
-  'Hawke\'s Bay Region': 'HB',
-  'Manawatu-Wanganui Region': 'MW',
-  'Marlborough Region': 'MB',
-  'Nelson Region': 'NS',
-  'Northland Region': 'NL',
-  'Otago Region': 'OT',
-  'Southland Region': 'SL',
-  'Taranaki Region': 'TK',
-  'Tasman Region': 'TM',
-  'Waikato Region': 'WA',
-  'Wellington Region': 'WE',
-  'West Coast Region': 'WC',
-  'No Region (Chatham Islands)': null
+export default (state_field) => {
+  const element = document.querySelector(state_field)
+
+  if (element && Array.from(element.querySelectorAll("option")).map((el) => el.value).filter((val) => val.length > 0)[0].length == 3) {
+    return {
+      'Auckland Region': 'AUK',
+      'Bay of Plenty Region': 'BOP',
+      'Canterbury Region': 'CAN',
+      'Gisborne Region': 'GIS',
+      'Hawke\'s Bay Region': 'HKB',
+      'Manawatu-Wanganui Region': 'MWT',
+      'Marlborough Region': 'MBH',
+      'Nelson Region': 'NSN',
+      'Northland Region': 'NTL',
+      'Otago Region': 'OTA',
+      'Southland Region': 'STL',
+      'Taranaki Region': 'TKI',
+      'Tasman Region': 'TAS',
+      'Waikato Region': 'WKO',
+      'Wellington Region': 'WGN',
+      'West Coast Region': 'WTC',
+      'No Region (Chatham Islands)': null
+    }
+  }
+
+  return {
+    'Auckland Region': 'AK',
+    'Bay of Plenty Region': 'BP',
+    'Canterbury Region': 'CT',
+    'Gisborne Region': 'GI',
+    'Hawke\'s Bay Region': 'HB',
+    'Manawatu-Wanganui Region': 'MW',
+    'Marlborough Region': 'MB',
+    'Nelson Region': 'NS',
+    'Northland Region': 'NL',
+    'Otago Region': 'OT',
+    'Southland Region': 'SL',
+    'Taranaki Region': 'TK',
+    'Tasman Region': 'TM',
+    'Waikato Region': 'WA',
+    'Wellington Region': 'WE',
+    'West Coast Region': 'WC',
+    'No Region (Chatham Islands)': null
+  }
 }
