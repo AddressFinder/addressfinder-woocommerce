6fb26bf0b5b0499e725291a00d55f2b475eaf9b1
Erik Fercak <erik.fercak@addressfinder.com>
2022-11-24 20:08:11 +1300

    ADF-1986 Fix NZ regions not populating
    The element we were checking for the NZ region mapping
    does not exist at the point in time of the check so we
    always defaulted to 2-letter regions.
    
    I have flipped the conditions around so that if the
    checked element is not there yet, or does not contain
    options with 2-letter values we return the 3-letter
    mapping.

--- dist/addressfinder.js
+++ dist/addressfinder.js
@@ -2889,34 +2889,26 @@ __webpack_require__.r(__webpack_exports__);
 // CONCATENATED MODULE: ./src/address_form_config/region_mappings.js
 /* harmony default export */ var region_mappings = (function (state_field) {
   var element = document.querySelector(state_field);
-
-  if (element && Array.from(element.querySelectorAll("option")).map(function (el) {
-    return el.value;
-  }).filter(function (val) {
-    return val.length > 0;
-  })[0].length == 3) {
-    return {
-      'Auckland Region': 'AUK',
-      'Bay of Plenty Region': 'BOP',
-      'Canterbury Region': 'CAN',
-      'Gisborne Region': 'GIS',
-      'Hawke\'s Bay Region': 'HKB',
-      'Manawatu-Wanganui Region': 'MWT',
-      'Marlborough Region': 'MBH',
-      'Nelson Region': 'NSN',
-      'Northland Region': 'NTL',
-      'Otago Region': 'OTA',
-      'Southland Region': 'STL',
-      'Taranaki Region': 'TKI',
-      'Tasman Region': 'TAS',
-      'Waikato Region': 'WKO',
-      'Wellington Region': 'WGN',
-      'West Coast Region': 'WTC',
-      'No Region (Chatham Islands)': null
-    };
-  }
-
-  return {
+  var three_letter_mapping = {
+    'Auckland Region': 'AUK',
+    'Bay of Plenty Region': 'BOP',
+    'Canterbury Region': 'CAN',
+    'Gisborne Region': 'GIS',
+    'Hawke\'s Bay Region': 'HKB',
+    'Manawatu-Wanganui Region': 'MWT',
+    'Marlborough Region': 'MBH',
+    'Nelson Region': 'NSN',
+    'Northland Region': 'NTL',
+    'Otago Region': 'OTA',
+    'Southland Region': 'STL',
+    'Taranaki Region': 'TKI',
+    'Tasman Region': 'TAS',
+    'Waikato Region': 'WKO',
+    'Wellington Region': 'WGN',
+    'West Coast Region': 'WTC',
+    'No Region (Chatham Islands)': null
+  };
+  var two_letter_mapping = {
     'Auckland Region': 'AK',
     'Bay of Plenty Region': 'BP',
     'Canterbury Region': 'CT',
@@ -2935,6 +2927,16 @@ __webpack_require__.r(__webpack_exports__);
     'West Coast Region': 'WC',
     'No Region (Chatham Islands)': null
   };
+
+  if (element && Array.from(element.querySelectorAll("option")).map(function (el) {
+    return el.value;
+  }).filter(function (val) {
+    return val.length > 0;
+  })[0].length == 2) {
+    return two_letter_mapping;
+  }
+
+  return three_letter_mapping;
 });
 // CONCATENATED MODULE: ./src/address_form_config/billing_address.js
 
@@ -3010,7 +3012,7 @@ __webpack_require__.r(__webpack_exports__);
     elements: {
       address1: '#billing-address_1',
       address2: null,
-      suburb: '#billing_address_2',
+      suburb: '#billing-address_2',
       city: '#billing-city',
       region: '#components-form-token-input-1',
       postcode: '#billing-postcode'
@@ -3021,7 +3023,7 @@ __webpack_require__.r(__webpack_exports__);
     countryValue: "Australia",
     elements: {
       address1: '#billing-address_1',
-      address2: null,
+      address2: '#billing-address_2',
       suburb: '#billing-city',
       state: '#components-form-token-input-1',
       postcode: '#billing-postcode'
