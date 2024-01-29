c278f90ca1a05a52b61850f805bc7d4296777572
Vincent Prigent <vincent.prigent1@gmail.com>
2024-01-30 11:54:14 +1300

    Update version in js files Build deploy version

--- dist/addressfinder.js
+++ dist/addressfinder.js
@@ -3080,7 +3080,7 @@ function woocommerce_plugin_createClass(Constructor, protoProps, staticProps) { 
     function WooCommercePlugin() {
       woocommerce_plugin_classCallCheck(this, WooCommercePlugin);
 
-      this.version = "1.5.9"; // Manages the mapping of the form configurations to the DOM.
+      this.version = "1.6.1"; // Manages the mapping of the form configurations to the DOM.
 
       this.PageManager = null; // Manages the form configurations, and creates any dynamic forms
 
--- src/woocommerce_plugin.js
+++ src/woocommerce_plugin.js
@@ -5,7 +5,7 @@ import { PageManager, MutationManager } from '@addressfinder/addressfinder-webpa
   class WooCommercePlugin {
     constructor() {
 
-      this.version = "1.5.9"
+      this.version = "1.6.1"
 
       // Manages the mapping of the form configurations to the DOM.
       this.PageManager = null
