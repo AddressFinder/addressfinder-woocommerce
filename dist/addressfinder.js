d0b53a3dfa0c67639751c65a6764b8035ebe0c42
Santosh Mani <santosh.mani@abletech.co.nz>
2024-08-13 14:12:58 +1200

    ADF-3567 - Bumped the plugin version to 1.7.5

--- dist/addressfinder.js
+++ dist/addressfinder.js
@@ -3568,7 +3568,7 @@ function woocommerce_plugin_createClass(Constructor, protoProps, staticProps) { 
     function WooCommercePlugin() {
       woocommerce_plugin_classCallCheck(this, WooCommercePlugin);
 
-      this.version = "1.7.3"; // Manages the mapping of the form configurations to the DOM.
+      this.version = "1.7.5"; // Manages the mapping of the form configurations to the DOM.
 
       this.PageManager = null; // Manages the form configurations, and creates any dynamic forms
 
