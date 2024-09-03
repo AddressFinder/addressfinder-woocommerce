71224edb05bc2555d4d4b6dd0fce51a88faa20f0
Erik Fercak <erik.fercak@addressfinder.com>
2024-09-03 16:58:56 +1200

    ADF-3574 Add support for client agent reporting

--- dist/addressfinder.js
+++ dist/addressfinder.js
@@ -4277,6 +4277,12 @@ var config_manager_ConfigManager = /*#__PURE__*/function () {
 var addressfinder_webpage_tools = __webpack_require__(0);
 
 // CONCATENATED MODULE: ./src/woocommerce_plugin.js
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { woocommerce_plugin_defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function woocommerce_plugin_defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function woocommerce_plugin_classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function woocommerce_plugin_defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -4381,20 +4387,22 @@ function woocommerce_plugin_createClass(Constructor, protoProps, staticProps) { 
     }, {
       key: "_initPlugin",
       value: function _initPlugin() {
-        var parsedWidgetOptions = this._safeParseJSONObject(w.AddressFinderConfig.widget_options);
-
-        var parsedNZWidgetOptions = this._safeParseJSONObject(w.AddressFinderConfig.nz_widget_options);
-
-        var parsedAUWidgetOptions = this._safeParseJSONObject(w.AddressFinderConfig.au_widget_options);
-
+        var parsedWidgetOptions = this._safeParseJSONObject(w.AddressFinderConfig.widget_options) || {};
+        var parsedNzWidgetOptions = this._safeParseJSONObject(w.AddressFinderConfig.nz_widget_options) || parsedWidgetOptions;
+        var parsedAuWidgetOptions = this._safeParseJSONObject(w.AddressFinderConfig.au_widget_options) || parsedWidgetOptions;
+        var parsedEvWidgetOptions = w.AddressFinderConfig.email || {};
+        var parsedPvWidgetOptions = w.AddressFinderConfig.phone || {};
+        var clientVersion = {
+          ca: "WooCommerce/".concat(this.version)
+        };
         var widgetConfig = {
           nzKey: w.AddressFinderConfig.key_nz || w.AddressFinderConfig.key || w.AddressFinderConfig.key_au,
           auKey: w.AddressFinderConfig.key_au || w.AddressFinderConfig.key || w.AddressFinderConfig.key_nz,
-          nzWidgetOptions: parsedNZWidgetOptions || parsedWidgetOptions || {},
-          auWidgetOptions: parsedAUWidgetOptions || parsedWidgetOptions || {},
-          intWidgetOptions: parsedWidgetOptions || {},
-          evWidgetOptions: w.AddressFinderConfig.email || {},
-          pvWidgetOptions: w.AddressFinderConfig.phone || {},
+          nzWidgetOptions: _objectSpread(_objectSpread({}, parsedNzWidgetOptions), clientVersion),
+          auWidgetOptions: _objectSpread(_objectSpread({}, parsedAuWidgetOptions), clientVersion),
+          intWidgetOptions: _objectSpread(_objectSpread({}, parsedWidgetOptions), clientVersion),
+          evWidgetOptions: _objectSpread(_objectSpread({}, parsedEvWidgetOptions), clientVersion),
+          pvWidgetOptions: _objectSpread(_objectSpread({}, parsedPvWidgetOptions), clientVersion),
           defaultCountry: w.AddressFinderConfig.default_country || 'nz',
           debug: w.AddressFinderConfig.debug || false
         };
