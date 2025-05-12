ab7e484bb0b9482d8b88ca65073819641d631360
Shane Mingins <shane.mingins@abletech.co.nz>
2025-05-13 11:18:56 +1200

    ADF-4279 - WooCommerce plugin v1.7.10 not activating for upgrades
     - added addressfinder_update_check() to execute addressfinder_initialize_defaults() if the version is being upgraded
     - added script to generate a developer zip that can be installed with WordPress and test changes

--- src/woocommerce_plugin.js
+++ src/woocommerce_plugin.js
@@ -5,7 +5,7 @@ import { PageManager, EmailPageManager, PhonePageManager, MutationManager } from
   class WooCommercePlugin {
     constructor() {
 
-      this.version = "1.7.10"
+      this.version = "1.7.11"
 
       // Manages the mapping of the form configurations to the DOM.
       this.PageManager = null
